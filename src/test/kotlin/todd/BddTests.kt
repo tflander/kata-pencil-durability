a9354ef11a5e906012aa5265720cb9bf4e33be30
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-05-28 18:53:48 -0400

    added kassert

---
+++ src/test/kotlin/todd/BddTests.kt
@@ -0,0 +1,11 @@
+package todd
+
+import org.junit.Test
+
+class BddTests {
+
+    @Test
+    fun `As a writer, I want to be able use a pencil to write text on a sheet of paper, so that I can better remember my thoughts*`() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
